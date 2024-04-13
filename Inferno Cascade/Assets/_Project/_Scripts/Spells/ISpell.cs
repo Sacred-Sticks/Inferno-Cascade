d433193a4819d98f4edb5265487b0839d6357fcd
Lucas Ackman <AckmanLD@gmail.com>
2024-04-13 00:04:45 -0500

    Added Spell Activation (with example spell for building more)

---
+++ Inferno Cascade/Assets/_Project/_Scripts/Spells/ISpell.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace Inferno_Cascade
+{
+    public interface ISpell
+    {
+        public static void BeginSpell(ISpell spell, Rigidbody body = null)
+        {
+            if (spell.RequiresBody)
+            {
+                spell.BeginSpell(body);
+                return;
+            }
+            spell.BeginSpell();
+        }
+        public bool RequiresBody { get; }
+
+        public void BeginSpell()
+        {
+
+        }
+        public void BeginSpell(Rigidbody body)
+        {
+
+        }
+        public void EndSpell()
+        {
+
+        }
+    }
+
+    public struct ExampleSpell : ISpell
+    {
+        public ExampleSpell(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+        public bool RequiresBody => false;
+
+        public void BeginSpell()
+        {
+            Debug.Log($"{Name} Used");
+        }
+    }
+}
--- Inferno Cascade/Assets/_Project/_Scripts/Spells/SpellManager.cs
+++ Inferno Cascade/Assets/_Project/_Scripts/Spells/SpellManager.cs
@@ -1,53 +1,118 @@
 using UnityEngine;
 using Kickstarter.Inputs;
+using Kickstarter.Observer;
 
-public class SpellManager : MonoBehaviour, IInputReceiver
+namespace Inferno_Cascade
 {
-    [Header("Input")]
-    [SerializeField] private FloatInput cycleWaterSpellInput;
-    [SerializeField] private FloatInput useWaterSpellInput;
-    [SerializeField] private FloatInput cycleFireSpellInput;
-    [SerializeField] private FloatInput useFireSpellInput;
+    public class SpellManager : Observable, IInputReceiver
+    {
+        [Header("Input")]
+        [SerializeField] private FloatInput cycleWaterSpellInput;
+        [SerializeField] private FloatInput useWaterSpellInput;
+        [SerializeField] private FloatInput cycleFireSpellInput;
+        [SerializeField] private FloatInput useFireSpellInput;
 
-    #region InputHandler
-    public void RegisterInputs(Player.PlayerIdentifier playerIdentifier)
-    {
-        cycleWaterSpellInput.RegisterInput(OnCycleWaterSpellInputChange, playerIdentifier);
-        cycleFireSpellInput.RegisterInput(OnCycleFireSpellInputChange, playerIdentifier);
-    }
+        private ISpell[] waterSpells;
+        private ISpell[] fireSpells;
+        private int waterSpellCount;
+        private int fireSpellCount;
+        private int waterSpellIndex;
+        private int fireSpellIndex;
 
-    public void DeregisterInputs(Player.PlayerIdentifier playerIdentifier)
-    {
-        cycleWaterSpellInput.DeregisterInput(OnCycleWaterSpellInputChange, playerIdentifier);
-        cycleFireSpellInput.DeregisterInput(OnCycleFireSpellInputChange, playerIdentifier);
-    }
+        #region UnityEvents
+        private void Start()
+        {
+            waterSpells = new ISpell[]
+            {
+                new ExampleSpell("Heal Spell"),
+                new ExampleSpell("Water Shield"),
+                new ExampleSpell("Water Jet"),
+            };
+            waterSpellCount = waterSpells.Length;
+            waterSpellIndex = 0;
+            fireSpells = new ISpell[]
+            {
+                new ExampleSpell("Fireball"),
+                new ExampleSpell("Fire Rune"),
+                new ExampleSpell("Fire Javelin"),
+            };
+            fireSpellCount = fireSpells.Length;
+            fireSpellIndex = 0;
+        }
+        #endregion
 
-    private void OnCycleWaterSpellInputChange(float input)
-    {
-        if (input == 0)
-            return;
-        Debug.Log("Water spell pressed");
-    }
-    
-    private void OnCycleFireSpellInputChange(float input)
-    {
-        if (input == 0)
-            return;
-        Debug.Log("Fire spell pressed");
-    }
+        #region InputHandler
+        public void RegisterInputs(Player.PlayerIdentifier playerIdentifier)
+        {
+            cycleWaterSpellInput.RegisterInput(OnCycleWaterSpellInputChange, playerIdentifier);
+            cycleFireSpellInput.RegisterInput(OnCycleFireSpellInputChange, playerIdentifier);
+            useWaterSpellInput.RegisterInput(OnUseWaterSpellInputChange, playerIdentifier);
+            useFireSpellInput.RegisterInput(OnUseFireSpellInputChange, playerIdentifier);
+        }
 
-    public void OnUseWaterSpellInput(float input)
-    {
-        if (input == 0)
-            return;
-        Debug.Log("Water spell used");
-    }
+        public void DeregisterInputs(Player.PlayerIdentifier playerIdentifier)
+        {
+            cycleWaterSpellInput.DeregisterInput(OnCycleWaterSpellInputChange, playerIdentifier);
+            cycleFireSpellInput.DeregisterInput(OnCycleFireSpellInputChange, playerIdentifier);
+            useWaterSpellInput.DeregisterInput(OnUseWaterSpellInputChange, playerIdentifier);
+            useFireSpellInput.DeregisterInput(OnUseFireSpellInputChange, playerIdentifier);
+        }
 
-    public void OnUseFireSpellInput(float input)
-    {
-        if (input == 0)
-            return;
-        Debug.Log("Fire spell used");
+        private void OnCycleWaterSpellInputChange(float input)
+        {
+            if (input == 0)
+                return;
+            waterSpellIndex++;
+            if (waterSpellIndex >= waterSpellCount)
+                waterSpellIndex = 0;
+            NotifyObservers(new CycleNotification(SpellType.Water, waterSpellIndex));
+        }
+
+        private void OnCycleFireSpellInputChange(float input)
+        {
+            if (input == 0)
+                return;
+            fireSpellIndex++;
+            if (fireSpellIndex >= fireSpellCount)
+                fireSpellIndex = 0;
+            NotifyObservers(new CycleNotification(SpellType.Fire, fireSpellIndex));
+        }
+
+        private void OnUseWaterSpellInputChange(float input)
+        {
+            if (input == 0)
+                ISpell.BeginSpell(waterSpells[waterSpellIndex]);
+            else
+                waterSpells[waterSpellIndex].EndSpell();
+        }
+
+        private void OnUseFireSpellInputChange(float input)
+        {
+            if (input == 0)
+                ISpell.BeginSpell(fireSpells[fireSpellIndex]);
+            else
+                fireSpells[fireSpellIndex].EndSpell();
+        }
+        #endregion
+
+        #region SubTypes
+        public enum SpellType
+        {
+            Water,
+            Fire,
+        }
+
+        public struct CycleNotification : INotification
+        {
+            public CycleNotification(SpellType spellType, int spellIndex)
+            {
+                SpellType = spellType;
+                SpellIndex = spellIndex;
+            }
+
+            public SpellType SpellType;
+            public int SpellIndex;
+        }
+        #endregion
     }
-    #endregion
 }
