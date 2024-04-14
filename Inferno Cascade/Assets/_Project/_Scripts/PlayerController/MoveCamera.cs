cbfc1fc2f34e5b2a8d6fa089965a3a9f1513880b
Daifaizon J Aldridge <daifaizonaldridge@gmail.com>
2024-04-13 19:54:56 -0500

    I made the base player controllers and interactions
    Sprite body made

---
+++ Inferno Cascade/Assets/_Project/_Scripts/PlayerController/MoveCamera.cs
@@ -0,0 +1,60 @@
+using Kickstarter.Inputs;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using UnityEngine;
+
+
+public class MoveCamera : MonoBehaviour, IInputReceiver
+{
+    [SerializeField] private Vector2Input rotationInput;
+    [SerializeField] float mouseSensitivity;
+
+    Vector2 rawInput;
+
+    private void Update()
+    {
+        RotatePlayer();
+        RotateCamera();
+    }
+
+    private void RotatePlayer()
+    {
+        //horizontel rotation
+        Transform root = transform.root;
+
+        float rotation = root.rotation.eulerAngles.y;
+        rotation += rawInput.x * mouseSensitivity;
+
+        root.localRotation = Quaternion.Euler(new Vector3(0, rotation, 0));
+
+    }
+
+    private void RotateCamera()
+    {
+        //vertical rotation
+
+        float rotation = transform.rotation.eulerAngles.x;
+        rotation -= rawInput.y * mouseSensitivity;
+
+        transform.localRotation = Quaternion.Euler(new Vector3(rotation, 0, 0));
+    }
+
+    public void RegisterInputs(Player.PlayerIdentifier playerIdentifier)
+    {
+        rotationInput.RegisterInput(OnRotationInputChange, playerIdentifier);
+    }
+
+    public void DeregisterInputs(Player.PlayerIdentifier playerIdentifier)
+    {
+        rotationInput.DeregisterInput(OnRotationInputChange, playerIdentifier);
+    }
+
+    private void OnRotationInputChange(Vector2 input)
+    {
+        rawInput = input;
+    }
+}
+        
+    
+
