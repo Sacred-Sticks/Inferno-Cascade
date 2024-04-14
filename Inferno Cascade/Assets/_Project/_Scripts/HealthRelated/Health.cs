7b767bca475dedb314f196407e12b93db10b23f2
Flame <deremo.noah@gmail.com>
2024-04-14 10:57:50 -0500

    hpbar code should work
    just has to get hooked up

--- Inferno Cascade/Assets/_Project/_Scripts/HealthRelated/Health.cs
+++ Inferno Cascade/Assets/_Project/_Scripts/HealthRelated/Health.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Kickstarter.Observer;
+using UnityEngine.UI;
 
 namespace Inferno_Cascade
 {
@@ -13,12 +14,26 @@ namespace Inferno_Cascade
     public class Health : Observable
     {
         [SerializeField] float hp,Maxhp;
+        [SerializeField] Image hpBar;
+        [SerializeField] bool IsPlayer;
+
+
+        public void Start()
+        {
+            if(IsPlayer)
+            {
+                hpBar=FindObjectOfType<hpLocator>().gameObject.GetComponent<Image>();
+            }
+        }
 
         public void changeHealth(float amount)
         {
             hp += amount;
             if (hp > Maxhp) { hp = Maxhp; }
             NotifyObservers(new HealthChange(hp));
+
+            if(hpBar !=null) { hpBar.fillAmount = hp / Maxhp; }
+
             if (hp <= 0)
             {
                 GetComponent<IOnDeath>().OnDeath();
---
+++ Inferno Cascade/Assets/_Project/_Scripts/HealthRelated/hpLocator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Inferno_Cascade
+{
+    public class hpLocator : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
